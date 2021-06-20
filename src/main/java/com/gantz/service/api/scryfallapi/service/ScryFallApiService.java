15d749b2f31b128358617873a81e561bc2fb010e
GantZo <putrify@mail.ru>
2021-06-20 11:51:52 +0300

    change: service to interface

--- src/main/java/com/gantz/service/api/scryfallapi/service/ScryFallApiService.java
+++ src/main/java/com/gantz/service/api/scryfallapi/service/ScryFallApiService.java
@@ -1,30 +1,11 @@
 package com.gantz.service.api.scryfallapi.service;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.*;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
 
-@Service
-public class ScryFallApiService {
+public interface ScryFallApiService {
 
-    private final RestTemplate restTemplate;
+    <T> ResponseEntity<T> get(String uri, Class<T> responseClass);
 
-    public ScryFallApiService(RestTemplateBuilder restTemplateBuilder, ScryFallApiErrorHandler handler) {
-        this.restTemplate = restTemplateBuilder.errorHandler(handler)
-                .build();
-    }
-
-    public <T> ResponseEntity<T> get(String uri, Class<T> responseClass) {
-        return restTemplate.getForEntity(ScryFallConstants.API_URI + uri, responseClass);
-    }
-
-    public <T> ResponseEntity<T> get(String uri, Class<T> responseClass, Object... variables) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity entity = new HttpEntity(headers);
-
-        return restTemplate.exchange(ScryFallConstants.API_URI + uri, HttpMethod.GET, entity, responseClass, variables);
-    }
+    <T> ResponseEntity<T> get(String uri, Class<T> responseClass, Object... variables);
 
 }
---
+++ src/main/java/com/gantz/service/api/scryfallapi/service/ScryFallApiServiceImpl.java
@@ -0,0 +1,34 @@
+package com.gantz.service.api.scryfallapi.service;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Primary
+public class ScryFallApiServiceImpl implements ScryFallApiService {
+
+    private final RestTemplate restTemplate;
+
+    public ScryFallApiServiceImpl(RestTemplateBuilder restTemplateBuilder, ScryFallApiErrorHandler handler) {
+        this.restTemplate = restTemplateBuilder.errorHandler(handler)
+                .build();
+    }
+
+    @Override
+    public <T> ResponseEntity<T> get(String uri, Class<T> responseClass) {
+        return restTemplate.getForEntity(ScryFallConstants.API_URI + uri, responseClass);
+    }
+
+    @Override
+    public <T> ResponseEntity<T> get(String uri, Class<T> responseClass, Object... variables) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity entity = new HttpEntity(headers);
+
+        return restTemplate.exchange(ScryFallConstants.API_URI + uri, HttpMethod.GET, entity, responseClass, variables);
+    }
+
+}
